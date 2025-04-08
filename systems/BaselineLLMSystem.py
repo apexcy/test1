818fb112e39134419b4f48b850f2c0d89663ac20
eugenieshine <yujinglai0707@gmail.com>
2025-04-08 17:13:45 -0400

    add generate_prompt() to ref sys

--- systems/BaselineLLMSystem.py
+++ systems/BaselineLLMSystem.py
@@ -2,7 +2,7 @@ from systems.generator_util import Generator, pdf_to_text
 from benchmark.benchmark_api import System
 import sys
 sys.path.append('./')
-from utils import * 
+from utils.baseline_utils import * 
 
 class BaselineLLMSystem(System):
     """
@@ -14,6 +14,110 @@ class BaselineLLMSystem(System):
         self.dataset_directory = None  # TODO(user): Update me
         self.model = model
         self.llm = Generator(model, verbose=self.verbose)
+
+        # Initialize directories for output and intermediate files
+        self.output_dir = None # to be set in run()
+        self.question_output_dir = None # to be set in run()
+        self.question_intermediate_dir = None # to be set in run()
+    
+    def generate_prompt(self, query:str) -> str:
+        """
+        Generate a prompt for the LLM based on the question.
+        :param question: Question object
+        :return: Prompt string
+        """
+        # Generate the RAG plan
+        # TODO: use process_dataset() to get the data
+        data = self.get_input_data()
+        file_names = list(self.dataset_directory.keys()) # get the file names from the dataset directory
+        file_paths = [os.path.join(self.dataset_directory, file) for file in file_names] # get the file paths
+
+        prompt = f"""
+        Your task is to answer the following question based on the provided data sources.
+        Question: {query}
+        Data file names: {file_names}
+
+        The following is a snippet of the data files:
+        {data}
+
+        Now think step-by-step carefully. 
+        First, provide a step-by-step reasoning of how you would arrive at the correct answer.
+        Do not assume the data files are clean or well-structured (e.g., missing values, inconsistent data type in a column).
+        Do not assume the data type of the columns is what you see in the data snippet (e.g., 2012 in Year could be a string, instead of an int). So you need to convert it to the correct type if your subsequent code relies on the correct data type (e.g., cast two columns to the same type before joining the two tables).
+        You have to consider the possible data issues observed in the data snippet and how to handle them.
+        Output the steps in a JSON format with the following keys:
+        - id: always "main-task" for the main task. For each subtask, use "subtask-1", "subtask-2", etc.
+        - query: the question the step is trying to answer. Copy down the question from above for the main task.
+        - data_sources: the data sources you need to check to answer the question. Include all the file names you need for the main task.
+        - subtasks: a list of subtasks. Each subtask should have the same structure as the main task.
+        For example, a JSON object for the task might look like this:
+        {{
+            "id": "main-task",
+            "query": "{query}",
+            "data_sources": ["file1.csv", "file2.csv"]
+            "subtasks": [{
+                "id": "subtask-1",
+                "query": "What is the exceedance rate in 2022?",
+                "data_sources": ["water-body-testing-2022.csv"]
+            },{
+                "id": "subtask-2",
+                "query": "What is the column name for the exceedance rate?",
+                "data_sources": ["water-body-testing-2022.csv"]
+            }]
+        }}
+        You can have multiple steps, and each step should be a JSON object.
+        Your output for this task should be a JSON array of JSON objects.
+        Mark the JSON array with ````json` and ````json` to indicate the start and end of the code block.
+
+        Then, provide the corresponding Python code to extract the answer from the data sources. 
+        The data sources you may need to answer the question are: {file_paths}.
+        
+        If possible, print the answer (in a JSON format) to each step you provided in the JSON array using the print() function.
+        Use "id" as the key to print the answer.
+        For example, if you have an answer to subtask-1, subtask-2, and main-task (i.e., the final answer), you should print it like this:
+        print(json.dumps(
+        {{"subtask-1": answer1, 
+        "subtask-2": answer2, 
+        "main-task": answer
+        }}, indent=4))
+        You can find a suitable indentation for the print statement. Always import json at the beginning of your code.
+
+        Mark the code with ````python` and ````python` to indicate the start and end of the code block.
+        """
+
+        # Save the prompt to a txt file
+        prompt_fp = os.path.join(self.question_output_dir, f"prompt.txt")
+        with open(prompt_fp, 'w') as f:
+            f.write(prompt)
+        print(f"Prompt saved to {prompt_fp}")
+        return prompt
+    
+    def execute_code(self, code_fp, try_number:int):
+        """
+        Execute the code in the file and save the output.
+        :param code_fp: Path to the code file
+        :return: Execution result
+        """
+        # Execute the code and save error messages
+        result = subprocess.run(
+            ['python', code_fp],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        # Save the printed output of the code execution
+        output_fp = os.path.join(self.question_intermediate_dir, f"pipeline-{try_number}_out.json")
+        with open(output_fp, 'w') as f:
+            # Clean NaN values to null for strict JSON compliance
+            stdout = clean_nan(result.stdout)
+            f.write(stdout)
+
+        # Save only compile/runtime errors
+        error_fp = os.path.join(self.question_intermediate_dir, f"errors-{try_number}.txt")
+        with open(error_fp, 'w') as f:
+            f.write(result.stderr)
+
+        return output_fp, error_fp
     
     def process_dataset(self, dataset_directory: str | os.PathLike) -> None:
         """
@@ -24,9 +128,7 @@ class BaselineLLMSystem(System):
         self.dataset = {}
         # read the files
         for file in os.listdir(dataset_directory):
-            if file.endswith(".pdf"):
-                self.dataset[file] = pdf_to_text(os.path.join(dataset_directory, file))
-            elif file.endswith(".csv"):
+            if file.endswith(".csv"):
                 self.dataset[file] = pd.read_csv(os.path.join(dataset_directory, file))
 
     def serve_query(self, query: str) -> dict | str:
