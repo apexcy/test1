859c49df04621340d7114a6c74cfca9eb8b20492
Siva Sudhir <sivaprasad2626@gmail.com>
2025-05-15 14:48:45 -0400

    llama3 biomedical results

--- exec_script.sh
+++ exec_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for workload in "archeology" "astronomy" "legal" "biomedical"; do
+for workload in "biomedical"; do
 
-for sut_name in "BaselineLLMSystemDeepseekR1" "BaselineLLMSystemLlama3_3Instruct"; do
+for sut_name in "BaselineLLMSystemLlama3_3Instruct"; do
  for config in "Naive" "OneShot" "FewShot"; do
     echo "Running workload: $workload with SUT: $sut_name$config"
     python evaluate.py --sut $sut_name$config --workload $workload.json --dataset_name $workload
