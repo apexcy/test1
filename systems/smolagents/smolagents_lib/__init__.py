4b4083d35dcdd8900bc77b6b063e4eea638e4d21
vitaglianog <vitaglianog@gmail.com>
2026-01-28 14:22:14 -0500

    Removed unnecessary files from smolagents fork

--- systems/smolagents/smolagents_lib/__init__.py
+++ systems/smolagents/smolagents_lib/__init__.py
@@ -19,13 +19,10 @@ __version__ = "1.18.0.dev0"
 from .agent_types import *  # noqa: I001
 from .agents import *  # Above noqa avoids a circular dependency due to cli.py
 from .default_tools import *
-from .gradio_ui import *
 from .local_python_executor import *
-from .mcp_client import *
 from .memory import *
 from .models import *
 from .monitoring import *
 from .remote_executors import *
 from .tools import *
 from .utils import *
-from .cli import *
